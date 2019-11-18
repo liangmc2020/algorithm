d65f439569ceb78c9fedc2ef8229d8ae59b2e389
梁铭琛 <564087014@qq.com>
2019-11-19 07:36:23 +0800

    twosum1

---
+++ leetcode/algorithm/twosum1.py
@@ -0,0 +1,23 @@
+
+class Solution:
+    def twoSum(self, nums, target):
+        ret = []
+        # length = len(nums)
+        for num in nums:
+            if target - num in nums:
+                if num*2 == target and nums.count(num) == 1:
+                    continue
+                else:
+                    return [nums.index(num),nums.index(target - num)]
+            else:
+                continue
+        
+        return ret
+
+if __name__ == "__main__":
+    nums = [2,7,11,15]
+    target = 9
+    ret = Solution().twoSum(nums,target)
+    for r in ret:
+        print(r)
+
